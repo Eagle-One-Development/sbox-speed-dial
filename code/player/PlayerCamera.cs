8771a5f37b87fae644e09565030fa16f2f885553
DoctorGurke <docgurk@gmail.com>
2021-05-24 21:08:20 +0200

    simple cam

--- code/player/PlayerCamera.cs
+++ code/player/PlayerCamera.cs
@@ -1,7 +1,26 @@
+using System;
 using Sandbox;
 
 namespace SpeedDial.Player {
-	public partial class SpeedDialCamera : ThirdPersonCamera {
+	public partial class SpeedDialCamera : Camera {
 
+		private readonly float CameraHeight = 200;
+
+		private readonly float CameraAngle = 60;
+
+		public override void Update() {
+			var pawn = Local.Pawn;
+
+			if(pawn == null)
+				return;
+
+			Pos = pawn.Position + Vector3.Up * CameraHeight + (Vector3.Forward * -(float)(CameraHeight * Math.Tan(CameraAngle)));
+
+			Rot = Rotation.FromAxis(Vector3.Left, CameraAngle);
+
+			FieldOfView = 90;
+
+			Viewer = null;
+		}
 	}
 }
