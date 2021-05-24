959e60de3d6acadcbf4cd226da9c4c6e78974406
Adesi <kopietz123@gmail.com>
2021-05-24 21:27:17 +0200

    Update PlayerCamera.cs

--- code/player/PlayerCamera.cs
+++ code/player/PlayerCamera.cs
@@ -1,28 +1,29 @@
 using System;
 using Sandbox;
 
-namespace SpeedDial.Player {
-	public partial class SpeedDialCamera : Camera {
+namespace SpeedDial.Player
+{
+	public partial class SpeedDialCamera : Camera
+	{
 
-		public virtual float CameraHeight => 350;
+		public virtual float CameraHeight => 400;
 
-		public virtual float CameraAngle => 60;
+		public virtual float CameraAngle => 75;
 
-		public virtual float CameraForwardOffset => 48; //my trigonometry might be fucked, but right now this makes it work nicely
-
-		public override void Update() {
+		public override void Update()
+		{
 			var pawn = Local.Pawn;
 
-			if(pawn == null)
+			if ( pawn == null )
 				return;
 
 			//DebugOverlay.Sphere(pawn.Position, 5, Color.Green, false);
 
-			Pos = pawn.Position + Vector3.Up * CameraHeight + (Vector3.Forward * -(float)(CameraHeight * Math.Tan(CameraAngle) + CameraForwardOffset));
+			Pos = pawn.Position + (Vector3.Up * CameraHeight) - ((Vector3.Forward * (-(float)(CameraHeight * Math.Tan( CameraAngle )))) / 2f);
 
-			Rot = Rotation.FromAxis(Vector3.Left, CameraAngle);
+			Rot = Rotation.FromAxis( Vector3.Left, CameraAngle );
 
-			FieldOfView = 90;
+			FieldOfView = 70;
 
 			Viewer = null;
 		}
