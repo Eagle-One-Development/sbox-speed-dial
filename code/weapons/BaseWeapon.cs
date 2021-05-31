d7f8e62034cb4a5e0715674eab5c7853cf74e24b
DoctorGurke <docgurk@gmail.com>
2021-05-31 20:10:55 +0200

    Weapon base v2

--- code/weapons/BaseWeapon.cs
+++ code/weapons/BaseWeapon.cs
@@ -9,12 +9,10 @@ using System.Threading.Tasks;
 using SpeedDial.Player;
 using SpeedDial.UI;
 namespace SpeedDial.Weapons {
-	public partial class BaseSpeedDialWeapon : BaseWeapon {
+	public partial class BaseSpeedDialWeapon : BaseCarriable {
 		public virtual AmmoType AmmoType => AmmoType.Pistol;
 		public virtual int ClipSize => 16;
 		public virtual float ReloadTime => 0.17f;
-		public virtual int Bucket => 1;
-		public virtual int BucketWeight => 100;
 
 		[Net]
 		public Entity previousOwner { get; set; }
@@ -23,19 +21,28 @@ namespace SpeedDial.Weapons {
 		public int AmmoClip { get; set; }
 
 		[Net, Predicted]
-		public TimeSince TimeSinceReload { get; set; }
-
-		[Net, Predicted]
-		public bool IsReloading { get; set; }
-
-		[Net, Predicted]
 		public TimeSince TimeSinceDeployed { get; set; }
 
 		public PickupTrigger PickupTrigger { get; protected set; }
 		TimeSince lifetime;
 
+		[Net, Predicted]
+		public TimeSince TimeSincePrimaryAttack { get; set; }
 
+		public virtual float PrimaryRate => 5.0f;
+		public virtual float SecondaryRate => 15.0f;
 		public virtual int AmmoToAward => 5;
+		public virtual int BulletCount => 1;
+		public virtual float BulletSpread => 0.1f;
+		public virtual float BulletForce => 1;
+		public virtual float BulletDamage => 100;
+		public virtual float BulletSize => 1;
+		public virtual bool Automatic => false;
+		public virtual string ShootSound => "rust_pistol.shoot";
+		public virtual string WorldModel => "models/weapons/sk_prop_pistol_01.vmdl";
+		public virtual Vector4 ScreenShakeParameters => new(1, 1, 1, 1);
+
+		public virtual int HoldType => 1;
 
 		public int AvailableAmmo() {
 			if(Owner is SpeedDialPlayer owner) {
@@ -53,7 +60,12 @@ namespace SpeedDial.Weapons {
 		public override void Spawn() {
 			base.Spawn();
 
-			SetModel("weapons/rust_pistol/rust_pistol.vmdl");
+			CollisionGroup = CollisionGroup.Weapon; // so players touch it as a trigger but not as a solid
+			SetInteractsAs(CollisionLayer.Debris); // so player movement doesn't walk into it
+
+			SetModel(WorldModel);
+
+			AmmoClip = ClipSize;
 
 			PickupTrigger = new();
 			PickupTrigger.Parent = this;
@@ -81,94 +93,88 @@ namespace SpeedDial.Weapons {
 				PhysicsBody.GravityScale = 1.0f;
 		}
 
-		public override void Reload() {
-			if(IsReloading)
-				return;
-
-			if(AmmoClip >= ClipSize)
-				return;
-
-			TimeSinceReload = 0;
-
-			if(Owner is SpeedDialPlayer player) {
-				if(player.AmmoCount(AmmoType) <= 0)
-					return;
-			}
-
-			IsReloading = true;
-
-			(Owner as AnimEntity).SetAnimBool("b_reload", true);
+		public override void SimulateAnimator(PawnAnimator anim) {
+			anim.SetParam("holdtype", HoldType);
+			anim.SetParam("aimat_weight", 1.0f);
 		}
 
 		public override void Simulate(Client owner) {
 
 			lifetime = 0;
 
-			if(TimeSinceDeployed < 0.6f)
+			if(!this.IsValid())
 				return;
 
-			if(!this.IsValid())
+			if(TimeSinceDeployed < 0.6f)
 				return;
 
 			if(CanPrimaryAttack()) {
 				TimeSincePrimaryAttack = 0;
 				AttackPrimary();
 			}
-
-			if(CanSecondaryAttack()) {
-				TimeSinceSecondaryAttack = 0;
-				AttackSecondary();
-			}
-
-			if(Owner != null) {
-				previousOwner = Owner;
-			}
-
-			if(TimeSinceDeployed < 0.6f)
-				return;
-
-			if(IsReloading && TimeSinceReload > ReloadTime) {
-				OnReloadFinish();
-			}
 		}
 
-		public virtual void OnReloadFinish() {
-			IsReloading = false;
+		public virtual bool CanPrimaryAttack() {
+			if(!Owner.IsValid() || (Automatic && !Input.Down(InputButton.Attack1)) || (!Automatic && !Input.Pressed(InputButton.Attack1))) return false;
+
+			var rate = PrimaryRate;
+			if(rate <= 0) return true;
+
+			return TimeSincePrimaryAttack > (1 / rate);
 		}
 
-		public override void AttackPrimary() {
-			//base.AttackPrimary
+		public virtual void AttackPrimary() {
 			TimeSincePrimaryAttack = 0;
-			TimeSinceSecondaryAttack = 0;
 
 			// Tell the clients to play the shoot effects
 			ShootEffects();
 
-			// ShootBullet is coded in a way where we can have bullets pass through shit
-			// or bounce off shit, in which case it'll return multiple results
-			foreach(var tr in TraceBullet(Owner.EyePos, Owner.EyePos + Owner.EyeRot.Forward * 5000)) {
+			PlaySound(ShootSound);
+
+			// shoot the bullets, bulletcount for something like a shotgun with multiple bullets
+			for(int i = 0; i < BulletCount; i++) {
+				ShootBullet(BulletSpread, BulletForce, BulletDamage, BulletSize);
+			}
+		}
+
+		public virtual void ShootBullet(float spread, float force, float damage, float bulletSize) {
+			var forward = Owner.EyeRot.Forward;
+			forward += (Vector3.Random + Vector3.Random + Vector3.Random + Vector3.Random) * spread * 0.25f;
+			forward = forward.Normal;
+
+			AmmoPanel.Current?.Bump();
+
+			foreach(var tr in TraceBullet(Owner.EyePos, Owner.EyePos + forward * 5000, bulletSize)) {
 				tr.Surface.DoBulletImpact(tr);
 
-				// var ps = Particles.Create("particles/physgun_beam.vpcf", tr.EndPos);
-				// ps.SetEntityAttachment(0, EffectEntity, "muzzle", true);
-				// ps.SetPos(0, Owner.EyePos + Vector3.Down * 20);
-				// ps.SetPos(1, tr.EndPos);
-
-				DebugOverlay.Line(Owner.EyePos + Vector3.Down * 20, tr.EndPos, Color.White, 1, false);
+				BulletTracer(EffectEntity.Position, tr.EndPos);
 
 				if(!IsServer) continue;
 				if(!tr.Entity.IsValid()) continue;
 
-				// We turn predictiuon off for this, so any exploding effects don't get culled etc
 				using(Prediction.Off()) {
-					var damage = DamageInfo.FromBullet(tr.EndPos, Owner.EyeRot.Forward * 100, 15)
+					var damageInfo = DamageInfo.FromBullet(tr.EndPos, forward * 100 * force, damage)
 						.UsingTraceResult(tr)
 						.WithAttacker(Owner)
 						.WithWeapon(this);
 
-					tr.Entity.TakeDamage(damage);
+					tr.Entity.TakeDamage(damageInfo);
 				}
 			}
+		}
+
+		public virtual IEnumerable<TraceResult> TraceBullet(Vector3 start, Vector3 end, float radius = 2.0f) {
+			bool InWater = Physics.TestPointContents(start, CollisionLayer.Water);
+
+			var tr = Trace.Ray(start, end)
+					.UseHitboxes()
+					.HitLayer(CollisionLayer.Water, !InWater)
+					.Ignore(Owner)
+					.Ignore(this)
+					.Size(radius)
+					.Run();
+
+			yield return tr;
 		}
 
 		[ClientRpc]
@@ -176,9 +182,10 @@ namespace SpeedDial.Weapons {
 			Host.AssertClient();
 
 			Particles.Create("particles/pistol_muzzleflash.vpcf", EffectEntity, "muzzle");
+			Particles.Create("particles/pistol_ejectbrass.vpcf", EffectEntity, "ejection_point");
 
 			if(IsLocalPawn) {
-				new Sandbox.ScreenShake.Perlin();
+				new Sandbox.ScreenShake.Perlin(ScreenShakeParameters.x, ScreenShakeParameters.y, ScreenShakeParameters.z, ScreenShakeParameters.w);
 			}
 
 			CrosshairPanel?.OnEvent("fire");
@@ -193,40 +200,6 @@ namespace SpeedDial.Weapons {
 			ps.SetPos(1, to);
 		}
 
-		public virtual void ShootBullet(float spread, float force, float damage, float bulletSize) {
-			var forward = Owner.EyeRot.Forward;
-			forward += (Vector3.Random + Vector3.Random + Vector3.Random + Vector3.Random) * spread * 0.25f;
-			forward = forward.Normal;
-
-			AmmoPanel.Current?.Bump();
-
-			//
-			// ShootBullet is coded in a way where we can have bullets pass through shit
-			// or bounce off shit, in which case it'll return multiple results
-			//
-
-			foreach(var tr in TraceBullet(Owner.EyePos, Owner.EyePos + forward * 5000, bulletSize)) {
-				tr.Surface.DoBulletImpact(tr);
-
-				BulletTracer(EffectEntity.Position, tr.EndPos);
-
-				if(!IsServer) continue;
-				if(!tr.Entity.IsValid()) continue;
-
-				//
-				// We turn predictiuon off for this, so any exploding effects don't get culled etc
-				//
-				using(Prediction.Off()) {
-					var damageInfo = DamageInfo.FromBullet(tr.EndPos, forward * 100 * force, damage)
-						.UsingTraceResult(tr)
-						.WithAttacker(Owner)
-						.WithWeapon(this);
-
-					tr.Entity.TakeDamage(damageInfo);
-				}
-			}
-		}
-
 		public bool TakeAmmo(int amount) {
 			if(AmmoClip < amount)
 				return false;
@@ -237,16 +210,6 @@ namespace SpeedDial.Weapons {
 
 		public void AwardAmmo() {
 			AmmoClip = Math.Clamp(AmmoClip + AmmoToAward, 0, ClipSize);
-		}
-
-		[ClientRpc]
-		public virtual void DryFire() {
-			// CLICK
-		}
-
-		public bool IsUsable() {
-			if(AmmoClip > 0) return true;
-			return AvailableAmmo() > 0;
 		}
 
 		public override void OnCarryStart(Entity carrier) {
--- code/weapons/Spatz12.cs
+++ code/weapons/Spatz12.cs
@@ -5,56 +5,14 @@ namespace SpeedDial.Weapons {
 	[Library("sd_spatz", Title = "Spatz-12")]
 	partial class Shotgun : BaseSpeedDialWeapon {
 		public override float PrimaryRate => 1;
-		public override float SecondaryRate => 1;
 		public override int ClipSize => 8;
-		public override int Bucket => 2;
-
-		public override void Spawn() {
-			base.Spawn();
-
-			SetModel("models/weapons/sk_shotgun.vmdl");
-
-			AmmoClip = 8;
-		}
-
-		public override void AttackPrimary() {
-			TimeSincePrimaryAttack = 0;
-			TimeSinceSecondaryAttack = 0;
-
-			if(!TakeAmmo(1)) {
-				DryFire();
-				return;
-			}
-
-			(Owner as AnimEntity).SetAnimBool("b_attack", true);
-
-			ShootEffects();
-			PlaySound("rust_pumpshotgun.shoot");
-			for(int i = 0; i < 6; i++) {
-				ShootBullet(0.6f, 0.3f, 100, 3.0f);
-			}
-		}
-
-
-		[ClientRpc]
-		protected override void ShootEffects() {
-			Host.AssertClient();
-
-			Particles.Create("particles/pistol_muzzleflash.vpcf", EffectEntity, "muzzle");
-			Particles.Create("particles/pistol_ejectbrass.vpcf", EffectEntity, "ejection_point");
-
-			ViewModelEntity?.SetAnimBool("fire", true);
-
-			if(IsLocalPawn) {
-				new Sandbox.ScreenShake.Perlin(1.0f, 1.5f, 2.0f);
-			}
-
-			CrosshairPanel?.OnEvent("fire");
-		}
-
-		public override void SimulateAnimator(PawnAnimator anim) {
-			anim.SetParam("holdtype", 2); // TODO this is shit
-			anim.SetParam("aimat_weight", 1.0f);
-		}
+		public override int HoldType => 2;
+		public override string WorldModel => "models/weapons/sk_shotgun.vmdl";
+		public override string ShootSound => "rust_pumpshotgun.shoot";
+		public override float BulletSpread => 0.6f;
+		public override float BulletForce => 0.3f;
+		public override float BulletDamage => 100;
+		public override float BulletSize => 3;
+		public override Vector4 ScreenShakeParameters => new(1, 1.5f, 2, 1);
 	}
 }
