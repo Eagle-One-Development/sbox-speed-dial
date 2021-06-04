fbd885be6c0712b2394f797518c63657e6ccdf97
genericicon <genericicon01@gmail.com>
2021-06-04 00:52:29 -0400

    Use temp bullet trail

--- code/weapons/BaseWeapon.cs
+++ code/weapons/BaseWeapon.cs
@@ -248,7 +248,7 @@ namespace SpeedDial.Weapons {
 
 		[ClientRpc]
 		protected virtual void BulletTracer(Vector3 from, Vector3 to) {
-			var ps = Particles.Create("particles/weapon_fx/bullet_trail.vpcf", to);
+			var ps = Particles.Create("particles/weapon_fx/bullet_trail_test.vpcf", to);
 			ps.SetPos(0, from);
 			ps.SetPos(1, to);
 		}
