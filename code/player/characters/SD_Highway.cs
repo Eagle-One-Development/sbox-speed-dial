7c9afc8f5b2477efcb3b441360009450233659aa
DoctorGurke <docgurk@gmail.com>
2021-06-06 08:24:35 +0200

    Sorry highway

--- code/player/characters/SD_Highway.cs
+++ code/player/characters/SD_Highway.cs
@@ -5,7 +5,7 @@ namespace SpeedDial.Player {
 	public class SD_Highway : BaseSpeedDialCharacter {
 		public override string Name => "Highway";
 		public override string Description => "Nobody knows who Highway is or what their goal is. Stories tell of a drifter who comes in and out of towns, leaving bodies in their wake.";
-		public override string Weapon => "sd_spatz";
+		public override string Weapon => "sd_shotgun";
 		public override string Portrait => "materials/ui/portraits/highway_hd.png";
 	}
 }
