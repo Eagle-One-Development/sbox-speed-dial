5ebee9f1f97dafeaf0ed0453bac64ef9d7cf1771
DoctorGurke <docgurk@gmail.com>
2021-06-06 08:25:02 +0200

    Sorry Maria

--- code/player/characters/SD_Maria.cs
+++ code/player/characters/SD_Maria.cs
@@ -5,7 +5,7 @@ namespace SpeedDial.Player {
 	public class SD_Maria : BaseSpeedDialCharacter {
 		public override string Name => "Maria";
 		public override string Description => "The former right hand of an L.A. crime gang, Maria now spends her days as muscle available to the highest bidder.";
-		public override string Weapon => "sd_tac";
+		public override string Weapon => "sd_rifle";
 		public override string Portrait => "materials/ui/portraits/maria_hd.png";
 	}
 }
