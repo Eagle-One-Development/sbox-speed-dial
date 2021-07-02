f6b3dc1f7f8003455e630c177c0d419bb4180e5d
Geeknerd1337 <geeknerd1337@gmail.com>
2021-07-01 19:30:54 -0500

    Update Pistol.cs

--- code/weapons/Pistol.cs
+++ code/weapons/Pistol.cs
@@ -5,11 +5,11 @@ namespace SpeedDial.Weapons {
 	[Library("sd_pistol", Title = "Glock-7")]
 	partial class Pistol : BaseSpeedDialWeapon {
 		public override float PrimaryRate => 12.0f;
-		public override int ClipSize => 18;
+		public override int ClipSize => 16;
 		public override float BulletSpread => 0.24f;
 		public override float BulletForce => 1.5f;
 		public override float BulletDamage => 105;
-		public override float BulletSize => 3;
+		public override float BulletSize => 4;
 		public override float VerticalBulletSpread => 0.25f;
 		public override int HoldType => 1;
 		public override string ShootSound => "sd_beretta";
