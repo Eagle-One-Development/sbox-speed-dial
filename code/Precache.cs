84a8f005d8c9048857757c3045fd1a706258dca0
Geeknerd1337 <geeknerd1337@gmail.com>
2021-07-04 16:52:14 -0500

    Update Precache.cs

--- code/Precache.cs
+++ code/Precache.cs
@@ -21,8 +21,72 @@ namespace SpeedDial {
 			Precache.Add("particles/weapon_fx/bullet_trail.vpcf");
 			Precache.Add("particles/pistol_ejectbrass.vpcf");
 
+
 			Precache.Add("animgraphs/sd_playermodel.vanmgrph");
 			Precache.Add("animgraphs/sd_playermodel_test.vanmgrph");
+
+			Precache.Add("sounds/music_onelesshero.vsnd");
+			Precache.Add("sounds/punch.vsnd");
+			Precache.Add("sounds/punch_2.vsnd");
+			Precache.Add("sounds/spin.vsnd");
+			Precache.Add("sounds/tape_noise.vsnd");
+			Precache.Add("sounds/tape_top.vsnd");
+			Precache.Add("sounds/woosh_1.vsnd");
+			Precache.Add("sounds/woosh_2.vsnd");
+			Precache.Add("sounds/spin.vsnd");
+			Precache.Add("sounds/drukqs/leaf.vsnd");
+			Precache.Add("sounds/drukqs/ollie_2.vsnd");
+			Precache.Add("sounds/drukqs/polvo.vsnd");
+			Precache.Add("sounds/drukqs/ritindi.vsnd");
+			Precache.Add("sounds/kill/draftsfx_killsecured_long (1).vsnd");
+			Precache.Add("sounds/kill/hit01.vsnd");
+			Precache.Add("sounds/music/[draft]speed-dial_main_theme_bmanedit.vsnd");
+			Precache.Add("sounds/music/[draft]speed-dial_main_theme_climax_10secondsearly_bmanedit.vsnd");
+			Precache.Add("sounds/music/music_onelesshero.vsnd");
+			Precache.Add("sounds/music/mx_speeddial_combat.vsnd");
+			Precache.Add("sounds/punch/punch03.vsnd");
+			Precache.Add("sounds/punch/punch04.vsnd");
+			Precache.Add("sounds/punch/woosh_2.vsnd");
+			Precache.Add("sounds/punch/woosh_1.vsnd");
+			Precache.Add("sounds/simpleguns/[draft]sfx_bat_hit_v2-001.vsnd");
+			Precache.Add("sounds/simpleguns/[draft]sfx_bat_hit_v2-002.vsnd");
+			Precache.Add("sounds/simpleguns/[draft]sfx_bat_hit_v2-003.vsnd");
+			Precache.Add("sounds/simpleguns/[draft1]sfx_dryfire.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_beegshotgun_sounds-001.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_beegshotgun_sounds-002.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_beegshotgun_sounds-003.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_beretta_shot-001.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_beretta_shot-002.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_beretta_shot-003.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_famas_shot-001.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_famas_shot-002.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_famas_shot-003.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_mac-10_shot-001.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_mac-10_shot-002.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_mac-10_shot-003.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_mac-10_shot-004.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_mac-10_shot-005.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_mac-10_shot-006.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_ra-23_shot-001.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_ra-23_shot-002.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_ra-23_shot-003.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_shotgun_sounds-001.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_shotgun_sounds-002.vsnd");
+			Precache.Add("sounds/simpleguns/[final1]sfx_shotgun_sounds-003.vsnd");
+			Precache.Add("sounds/simpleguns/descrow_shot_noise.vsnd");
+			Precache.Add("sounds/ui/[draft]sfx_killsecured_long.vsnd");
+			Precache.Add("sounds/ui/[draft]sfx_killsecured_short.vsnd");
+			Precache.Add("sounds/ui/click.vsnd");
+			Precache.Add("sounds/ui/tape_noise.vsnd");
+			Precache.Add("sounds/ui/tape_stop.vsnd");
+			Precache.Add("sounds/ui/tape_top.vsnd");
+			Precache.Add("sounds/weapon_fx/spin.vsnd");
+
+
+			
+
+
+
 
 		}
 	}
