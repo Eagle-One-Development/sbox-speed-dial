743987c071099c09cf2645b5a81987d1c9f11fd5
genericicon <genericicon01@gmail.com>
2021-07-09 23:16:19 -0400

    Roomclearer uses old shotgun model as temp

--- code/weapons/RoomClearer.cs
+++ code/weapons/RoomClearer.cs
@@ -7,7 +7,7 @@ namespace SpeedDial.Weapons {
 		public override float PrimaryRate => 1;
 		public override int ClipSize => 2;
 		public override int HoldType => 4;
-		public override string WorldModel => "models/weapons/shotgun/prop_shotgun.vmdl";
+		public override string WorldModel => "models/weapons/shotgun/prop_roomclearer.vmdl";
 		public override string ShootSound => "sd_beeg";
 		public override float BulletSpread => 0.8f;
 		public override float BulletForce => 0.3f;
@@ -15,7 +15,7 @@ namespace SpeedDial.Weapons {
 		public override float BulletSize => 3;
 		public override int BulletCount => 16;
 		public override float Range => 1024;
-		public override string AttachementName => "shotgun_attach";
+		public override string AttachementName => "roomclearer_attachment";
 		public override float VerticalBulletSpread => 0.2f;
 		public override string EjectionParticle => "particles/weapon_fx/shotgun_ejectbrass.vpcf";
 	}
