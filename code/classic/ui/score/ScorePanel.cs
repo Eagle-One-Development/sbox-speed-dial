a6ebbf5dd8ddf1942c3df06fe40c27a0354eb17c
DoctorGurke <docgurk@gmail.com>
2021-11-28 01:09:34 +0100

    combo intensity gradients

--- code/classic/ui/score/ScorePanel.cs
+++ code/classic/ui/score/ScorePanel.cs
@@ -11,6 +11,7 @@ namespace SpeedDial.Classic.UI {
 		private readonly Panel Combo;
 		private readonly Label ScoreLabel;
 		private readonly Label ComboLabel;
+		private readonly Panel IntensityPanel;
 		private float ComboScale = 1;
 		private float _combo = 0;
 		private float _score = 0;
@@ -25,37 +26,53 @@ namespace SpeedDial.Classic.UI {
 
 			StyleSheet.Load("/classic/ui/score/ScorePanel.scss");
 			AddClass("scorepanel");
-			Score = Add.Panel("score");
+
+			// TODO: put this in it's own thing to make sure it's behind stuff?
+			IntensityPanel = Add.Panel("intensity");
+			IntensityPanel.Add.Panel("right");
+			IntensityPanel.Add.Panel("left");
+
+			var pan = Add.Panel("scoreCombo");
+			Score = pan.Add.Panel("score");
 			ScoreLabel = Score.Add.Label("0 PTS", "scorelabel");
 
-			Combo = Add.Panel("combo");
+			Combo = pan.Add.Panel("combo");
 			ComboLabel = Combo.Add.Label("x0", "combolabel");
 		}
 
 		public override void Tick() {
-			var score = Local.Client.GetValue("score", 0);
-			_score = _score.LerpTo(score, Time.Delta * 7f);
-			//ScoreLabel.Text = $"{(int)(_score + 0.5f)} PTS";
-			ScoreLabel.Text = $"{score} PTS";
-
-			var combo = Local.Client.GetValue("combo", 0);
-			_combo = _combo.LerpTo(combo, Time.Delta * 10f);
-			ComboLabel.Text = $"x{(int)(_combo + 0.5f)}";
-
-			// combo text scaling + rotating for intensity that scales with combo amount
-			PanelTransform transform = new();
-			// clamp it so it doesn't go ape mode on higher combos
-			var _effectMultiplier = combo.Clamp(0, 25);
-			transform.AddScale(ComboScale + 0.05f * _combo + ((MathF.Sin(Time.Now * _effectMultiplier) + 1) / 2 * combo * 0.05f));
-			transform.AddRotation(0, 0, MathF.Sin(Time.Now * _effectMultiplier * 1.234f) * _effectMultiplier * 0.5f);
-			ComboLabel.Style.Transform = transform;
-
-			if(combo <= 0) {
-				ComboScale = ComboScale.LerpTo(0, Time.Delta * 3f);
-				return;
+			// score
+			{
+				var score = Local.Client.GetValue("score", 0);
+				_score = _score.LerpTo(score, Time.Delta * 7f);
+				//ScoreLabel.Text = $"{(int)(_score + 0.5f)} PTS";
+				ScoreLabel.Text = $"{score} PTS";
 			}
 
-			ComboScale = ComboScale.LerpTo(1, Time.Delta * 7f);
+			// combo
+			{
+				var combo = Local.Client.GetValue("combo", 0);
+				_combo = _combo.LerpTo(combo, Time.Delta * 10f);
+				ComboLabel.Text = $"x{(int)(_combo + 0.5f)}";
+
+				// intensity gradients at the left and right side
+				IntensityPanel.Style.Opacity = _combo.Clamp(0, 25) * 0.02f; // 25 * 0.04 = 0.5
+
+				// combo text scaling + rotating for intensity that scales with combo amount
+				PanelTransform transform = new();
+				// clamp it so it doesn't go ape mode on higher combos
+				var _effectMultiplier = combo.Clamp(0, 25);
+				transform.AddScale(ComboScale + 0.05f * _combo + ((MathF.Sin(Time.Now * _effectMultiplier) + 1) / 2 * combo * 0.05f));
+				transform.AddRotation(0, 0, MathF.Sin(Time.Now * _effectMultiplier * 1.234f) * _effectMultiplier * 0.5f);
+				ComboLabel.Style.Transform = transform;
+
+				if(combo <= 0) {
+					ComboScale = ComboScale.LerpTo(0, Time.Delta * 3f);
+					return;
+				}
+
+				ComboScale = ComboScale.LerpTo(1, Time.Delta * 7f);
+			}
 		}
 	}
 }
