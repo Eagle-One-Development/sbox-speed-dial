27aeaaccbe5b22687537ad53ded1b1d4c383e6ca
DoctorGurke <docgurk@gmail.com>
2021-12-03 02:32:05 +0100

    character asset update

--- code/classic/player/Classic.Character.cs
+++ code/classic/player/Classic.Character.cs
@@ -6,11 +6,12 @@ using Hammer;
 namespace SpeedDial.Classic.Player {
 	[Library("sdgchar"), AutoGenerate]
 	public partial class Character : Asset {
-		public string CharacterName { get; set; }
-		public string Description { get; set; }
-		public string WeaponClass { get; set; }
-		[ResourceType("png")] public string Portrait { get; set; }
-		[ResourceType("vmdl")] public string Model { get; set; }
+		public string CharacterName { get; set; } = "name";
+		public string Description { get; set; } = "desc";
+		public string WeaponClass { get; set; } = "sdg_pistol";
+		[ResourceType("png")] public string Portrait { get; set; } = "materials/ui/portraits/jack_hd.jpg";
+		[ResourceType("png")] public string Weapon { get; set; } = "materials/ui/weapons/pistol.jpg";
+		[ResourceType("vmdl")] public string Model { get; set; } = "models/playermodels/character_jack.vmdl";
 
 		[Skip]
 		public static List<Character> All = new();
@@ -18,12 +19,17 @@ namespace SpeedDial.Classic.Player {
 		[Skip]
 		public Texture PortraitTexture { get; private set; }
 		[Skip]
+		public Texture WeaponTexture { get; private set; }
+		[Skip]
 		public Model CharacterModel { get; private set; }
 
 		protected override void PostLoad() {
 			Portrait = System.IO.Path.ChangeExtension(Portrait, "png");
-			if(Host.IsClient)
+			Weapon = System.IO.Path.ChangeExtension(Weapon, "png");
+			if(Host.IsClient) {
 				PortraitTexture = Texture.Load(Portrait);
+				WeaponTexture = Texture.Load(Weapon);
+			}
 
 			CharacterModel = Sandbox.Model.Load(Model);
 
