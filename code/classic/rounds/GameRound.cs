e72fcec675edee8419a14bc0df9f3b2c4cfeba41
DoctorGurke <docgurk@gmail.com>
2022-01-14 13:10:38 +0100

    round end climax

--- code/classic/rounds/GameRound.cs
+++ code/classic/rounds/GameRound.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 using Sandbox;
 
@@ -7,7 +8,7 @@ using SpeedDial.Classic.Player;
 
 namespace SpeedDial.Classic.Rounds {
 	public partial class GameRound : TimedRound {
-		public override TimeSpan RoundDuration => TimeSpan.FromMinutes(5);
+		public override TimeSpan RoundDuration => TimeSpan.FromMinutes(1);
 		public override string RoundText => "Round active.";
 
 		protected override void OnStart() {
@@ -18,11 +19,24 @@ namespace SpeedDial.Classic.Rounds {
 
 				pawn.Frozen = false;
 			}
+
+			// start climax track 10 seconds before round ends
+			_ = PlayClimaxMusic((int)RoundDuration.TotalSeconds - 10);
 		}
+
+		
 
 		protected override void OnFinish() {
 			base.OnFinish();
 			Game.Current.ActiveGamemode?.SetRound(new PostRound());
+		}
+
+		private async Task PlayClimaxMusic(int delay) {
+			await GameTask.DelaySeconds(delay);
+			foreach(var client in Client.All.Where(x => x.Pawn is ClassicPlayer)) {
+				var pawn = client.Pawn as ClassicPlayer;
+				pawn.PlayRoundendClimax(To.Single(client));
+			}
 		}
 	}
 }
