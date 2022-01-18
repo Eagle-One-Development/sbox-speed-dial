699b562bf86e50e32fd2d77d83d2af1a8d5c19d6
DoctorGurke <docgurk@gmail.com>
2022-01-18 23:55:07 +0100

    hide score panel in warmup and post round

--- code/classic/ui/Hud.Classic.cs
+++ code/classic/ui/Hud.Classic.cs
@@ -19,6 +19,8 @@ namespace SpeedDial.Classic.UI {
 			SetClass("state-visible-game", ClassicGamemode.Current.ActiveRound is not PostRound);
 			// cursor only shows when using mouse input
 			SetClass("state-visible-cursor", !Input.UsingController);
+			// hide score in warmup AND post round
+			SetClass("state-visible-warmup", ClassicGamemode.Current.ActiveRound is not WarmupRound or PostRound);
 		}
 	}
 }
