381d9f7e7ca9dc6cb8666a3883b61d47bcd9bc0f
DoctorGurke <docgurk@gmail.com>
2022-01-31 23:20:19 +0100

    Create Events.cs

---
+++ code/base/events/Events.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sandbox;
+
+namespace SpeedDial {
+	public static class SpeedDialEvent {
+		public static class Gamemode {
+			public class ResetAttribute : EventAttribute {
+				public ResetAttribute() : base("sd.gamemode.reset") {}
+			}
+
+			public class StartAttribute : EventAttribute {
+				public StartAttribute() : base("sd.gamemode.start") { }
+			}
+
+			public class EndAttribute : EventAttribute {
+				public EndAttribute() : base("sd.gamemode.end") { }
+			}
+		}
+	}
+}
