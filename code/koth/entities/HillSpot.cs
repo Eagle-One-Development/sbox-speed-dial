df4ce65a882dd58c215528572b920f16a9f9eb7b
Geeknerd1337 <geeknerd1337@gmail.com>
2022-02-06 14:43:49 -0600

    Koth Work

---
+++ code/koth/entities/HillSpot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+
+namespace Sandbox.koth.entities {
+	public class HillSpot : ModelEntity {
+		public override void Spawn() {
+			base.Spawn();
+			SetModel("models/koth/ring.vmdl");
+			Transmit = TransmitType.Always;
+		}
+	}
+}
---
+++ code/koth/entities/HillSpotSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpeedDial;
+namespace Sandbox.koth.entities {
+	[Library("sd_koth_hill_spawn", Title = "Random Hill Site Spawn")]
+	[Hammer.EditorModel("models/koth/ring.vmdl")]
+	public partial class HillSpotSpawn : GamemodeEntity {
+		public override void Spawn() {
+			base.Spawn();
+			Transmit = TransmitType.Always;
+		}
+	}
+}
