ecb82bff1097e6b1875e1896eab46efa36d3ca31
DoctorGurke <docgurk@gmail.com>
2022-02-11 18:19:36 +0100

    don't inherit oc_gun from pistol so it doesn't count as a spawnable

--- code/onechamber/weapons/OneChamber.Gun.cs
+++ code/onechamber/weapons/OneChamber.Gun.cs
@@ -10,7 +10,17 @@ using SpeedDial.Classic.Weapons;
 
 namespace SpeedDial.OneChamber.Weapons {
 	[Library("oc_gun")]
-	public partial class Gun : Pistol {
-
+	public partial class OneChamberGun : ClassicBaseWeapon {
+		public override float PrimaryRate => 12.0f;
+		public override int ClipSize => 16;
+		public override float BulletSpread => 0.24f;
+		public override float BulletForce => 1.5f;
+		public override float BulletDamage => 105;
+		public override float BulletSize => 4;
+		public override float VerticalBulletSpread => 0.25f;
+		public override int HoldType => 2;
+		public override string ShootSound => "sd_pistol.shoot";
+		public override string WorldModel => "models/weapons/pistol/prop_pistol.vmdl";
+		public override string AttachementName => "pistol_attach";
 	}
 }
