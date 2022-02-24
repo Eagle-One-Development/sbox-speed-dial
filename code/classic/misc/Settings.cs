97d4bbca6e03d36a5c124825f0f3451134d2a95c
Doctor Gurke <docgurk@gmail.com>
2022-02-24 14:04:39 +0100

    instantly stop music when setting sd_music to false during a round

--- code/classic/misc/Settings.cs
+++ code/classic/misc/Settings.cs
@@ -1,8 +1,19 @@
 ﻿namespace SpeedDial.Classic;
 
+using SpeedDial.Classic.Player;
+
 public static partial class Settings {
 	[ClientVar("sd_viewshift_toggle")]
 	public static bool ViewshiftToggle { get; set; } = false;
+
+	// Music enable/disable
 	[ClientVar("sd_music")]
-	public static bool MusicEnabled { get; set; } = true;
+	public static bool MusicEnabled { get { return _musicEnabled; } set { OnMusicEnabledChanged(value); _musicEnabled = value; } }
+	private static bool _musicEnabled = true;
+
+	private static void OnMusicEnabledChanged(bool value) {
+		if(!value && Local.Pawn is ClassicPlayer player) {
+			player.StopSoundtrack(true);
+		}
+	}
 }
