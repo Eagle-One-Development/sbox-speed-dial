61f90b716c8cafa8b51afe0cfd0f210856b11bbc
DoctorGurke <docgurk@gmail.com>
2022-11-23 20:33:01 +0100

    add sdstrman for precaching

---
+++ code/base/resources/StringManifest.cs
@@ -0,0 +1,8 @@
+﻿namespace SpeedDial;
+
+[GameResource( "Speed-Dial String Manifest", "sdstrman", "desc" )]
+public class StringManifest : GameResource
+{
+	[Property]
+	public List<string> Manifest { get; set; }
+}
