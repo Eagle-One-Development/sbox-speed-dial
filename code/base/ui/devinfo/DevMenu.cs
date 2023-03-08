0d74b944970c1ba2e7197d544b087f9b616b8e50
Doctor Gurke <docgurk@gmail.com>
2023-03-08 12:36:31 +0100

    upgrade DevMenu to razor

--- code/base/ui/devinfo/DevMenu.cs
+++ code/base/ui/devinfo/DevMenu.cs
@@ -1,15 +1,9 @@
-﻿using Sandbox.UI;
-using Sandbox.UI.Construct;
+﻿namespace SpeedDial;
 
-namespace SpeedDial;
-
-[UseTemplate]
-public partial class DevMenu : Panel
+public partial class DevMenu
 {
 	public override void Tick()
 	{
-		base.Tick();
-
 		SetClass( "open", Game.LocalClient.IsListenServerHost && Input.Down( InputButton.View ) );
 	}
 }
