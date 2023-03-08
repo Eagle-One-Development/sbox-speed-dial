8dfd23b4e73d13e6e628daaac7e27f25e7289650
Doctor Gurke <docgurk@gmail.com>
2023-03-08 12:34:08 +0100

    upgrade DevInfo to razor

--- code/base/ui/devinfo/DevInfo.cs
+++ code/base/ui/devinfo/DevInfo.cs
@@ -1,12 +1,19 @@
 ﻿namespace SpeedDial;
 
-[UseTemplate]
-public partial class DevInfo : Panel
+/// <summary>
+/// IN-DEV/PLAYTEST watermark with timestamp in bottom right of the screen.
+/// </summary>
+public partial class DevInfo
 {
-	private string Info { get; set; }
+	public string Info { get; set; }
 	public override void Tick()
 	{
 		SetClass( "visible", Debug.Playtest || Debug.Enabled );
+
+		var old = Info;
 		Info = $"{(Debug.Playtest ? "PLAYTEST" : "IN-DEV")} {DateTime.Now:dd.MM.yyyy}";
+
+		if ( Info != old )
+			StateHasChanged();
 	}
 }
